75a0e96d7f5b701d6c2d3df554c8e5a8a1204637
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-06-29 21:42:09 +0200

    Add new category
    add new category for product

--- include/productAdd.php
+++ include/productAdd.php
@@ -9,23 +9,22 @@ include_once 'include/dbConn.php';
             <td>ID</td>
             <td>Tytuł</td>
             <td>Opis</td>
+            <td>EAN</td>
             <td>Status</td>
-            <!-- <td>Jednostka(W-F1.1)</td>
-            <td>Ilość(W-F1.1)</td>
-            <td>Cena(W-F1.1)</td>
-            <td>Stawka VAT(W-F1.1)</td> -->
+            <td>Ilość</td>
+            <td>Jednostka</td>
+            <td>Cena</td>
+            <td>Stawka VAT</td>
         </tr>
 
         <td>
             <?php //id pobierane na podstawie ostatnio dodanego produktu 
             $sql = "SELECT MAX(product_id) FROM product;";
             if ($resultProducts = mysqli_query($conn, $sql)) {
-
                 while ($row = mysqli_fetch_row($resultProducts)) {
                     echo ($row[0] + 1);
-                }
-            }
-            ?>
+            }  }
+            ?>   
         </td> 
         <td>
             <input type='text' name='productTitle'>
@@ -33,13 +32,39 @@ include_once 'include/dbConn.php';
         <td>
             <input type='text' name='productDescribe'>
         </td>
+        <td>
+            <input type='text' name='productEAN'>
+        </td>
+       
         <td> <select name='productStatus'>
                 <option value='dostepny'>dostępny</option>
                 <option value='niedostepny'>niedosępny</option>
             </select>
         </td>
+        <td>
+            <input type='number' name='productQuantity'>
+        </td>
+       
+             <td> <select name='product_unit'> 
+                <option value='sztuka'>sztuka</option>
+                <option value='kilogram'>kilogram</option>
+                <option value='gram'>gram</option>
+                <option value='metr'>metr</option>
+                <option value='litr'>litr</option>
+            </select>
+        </td>
+    <td>
+<input type="number" name="">
+    </td>
+    <td>
+    <input type="number" name="product_price">
+    </td>
+    <td>
+    <input type="text" name="product_VAT">
+    </td>
+    
+    </tr>
         <tr>
-            <td></td>
             <td><input type='submit' value="zatwierdź" name='zatwierdz'></td>
 
         </tr>
