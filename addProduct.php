e30cfacfe183a1d6b8eaf05349b351796640d7a4
WojtekFront <wojciech_lisowski@wp.pl>
2022-08-08 21:26:57 +0200

    add  product

---
+++ addProduct.php
@@ -0,0 +1,25 @@
+<?php
+define('title', 'magazyn');
+include_once 'include/incHeader.php';
+include_once 'include/dbConn.php';
+
+    if (isset($_SESSION["userid"])) {
+
+
+        // <div id="addProduct">
+
+        include_once 'include/productAdd.php';
+
+        // </div>
+
+ 
+
+
+
+        include 'include/admLog.php';
+    } else {
+        echo "Zaloguj się, aby mieć możliwość edycji panelu.";
+    }
+
+include_once 'include/incFooter.php';
+?>
