76b7d6895a5f1b25065c3eb2ad652abdba8bfc0c
WojtekFront <wojciech_lisowski@wp.pl>
2022-08-11 08:29:28 +0200

    edit add product

--- include/productAdd2.php
+++ include/productAdd2.php
@@ -4,19 +4,28 @@ include_once 'include/dbConn.php';
 ?>
 <h3>Dodawanie Produktu <span class="badge badge-secondary">wersja beta</span></h3>
 <form class="editPosition" method="POST" action="include/dbInsert.php">
+  <!-- productTitle -->
   <div class="form-group row">
     <label for="productTitle" class="col-sm-2 col-form-label">Nazwa towaru:</label>
     <div class="col-sm-6 col-md-4">
       <input type="text" class="form-control" id="productTitle" name="productTitle" placeholder="nazwa">
     </div>
   </div>
-
+  <!-- productDescribe -->
   <div class="form-group row">
     <label for="productDescribe" class="col-sm-2 col-form-label">Opis produktu:</label>
     <div class="col-sm-6 col-md-6">
       <textarea type="text" class="form-control" id="productDescribe" name="productDescribe" placeholder="opis" rows="5"></textarea>
     </div>
   </div>
+    <!-- productEAN -->
+    <div class="form-group row">
+    <label for="productEAN" class="col-sm-2 col-form-label">Kod produktu/EAN:</label>
+    <div class="col-sm-6 col-md-4">
+      <input type="text" class="form-control" id="productEAN" name="productEAN" placeholder="kod">
+    </div>
+  </div>
+  <!-- productStatus -->
   <div class="form-group row">
     <label for="productStatus" class="col-sm-2 col-form-label">Status produktu</label>
     <div class="col-sm-6 col-md-4">
@@ -30,12 +39,11 @@ include_once 'include/dbConn.php';
       </div>
     </div>
   </div>
-
+  <!-- productUnit -->
   <div class="form-group row">
     <label for="productUnit" class="col-sm-2 col-form-label">Jednostka miary:</label>
     <div class="col-sm-1 col-md-1">
-      <!-- <input type="text" class="form-control" id="vatProduct" name="vatProduct" placeholder=""> -->
-      <select class="form-control" id="vatProduct" name="vatProduct">
+      <select class="form-control" id="productUnit" name="productUnit">
         <option>sztuka</option>
         <option>kilogram</option>
         <option>gram</option>
@@ -44,22 +52,24 @@ include_once 'include/dbConn.php';
       </select>
     </div>
   </div>
+  <!-- productQuantity -->
   <div class="form-group row">
     <label for="productQuantity" class="col-sm-2 col-form-label">Ilość towaru:</label>
     <div class="col-sm-6 col-md-4">
-      <input type="number" class="form-control" id="productQuantity" name="productQuantity" placeholder="">
+      <input type="number" class="form-control" id="productQuantity" name="productQuantity" placeholder="0">
     </div>
   </div>
+  <!-- productPrice -->
   <div class="form-group row">
-    <label for="productPrice" class="col-sm-2 col-form-label">Cena produktu:</label>
+    <label for="productPriceNet" class="col-sm-2 col-form-label">Cena produktu:</label>
     <div class="col-sm-6 col-md-4">
-      <input type="number" class="form-control" id="productPrice" name="productPrice" placeholder="cena">
+      <input type="number" class="form-control" id="productPriceNet" name="productPriceNet" placeholder="cena" step=0.01>
     </div>
   </div>
+  <!-- vatProduct -->
   <div class="form-group row">
     <label for="vatProduct" class="col-sm-2 col-form-label">Stawka VAT:</label>
     <div class="col-sm-1 col-md-1">
-      <!-- <input type="text" class="form-control" id="vatProduct" name="vatProduct" placeholder=""> -->
       <select class="form-control" id="vatProduct" name="vatProduct">
         <option>23%</option>
         <option>8%</option>
@@ -69,11 +79,13 @@ include_once 'include/dbConn.php';
       </select>
     </div>
   </div>
+  <!-- productCategory -->
   <div class="form-group row">
     <label for="productCategory" class="col-sm-2 col-form-label">Kategoria produktu:</label>
     <div class="col-sm-6 col-md-4">
-      <input type="text" class="form-control" id="productCategory" name="productCategory" placeholder="" list="categoryList">
-      <datalist id="categoryList"> <!-- extract category to external file  -->
+      <input type="text" class="form-control" id="productCategory" name="productCategory" placeholder="wpisz lub wybierz" list="categoryList">
+      <datalist id="categoryList">
+        <!-- extract category to external file  -->
         <option value="żywność">
         <option value="odzież">
         <option value="art.higieniczne">
